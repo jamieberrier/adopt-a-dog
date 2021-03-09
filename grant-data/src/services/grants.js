962551027a9bbcb3e66ef71b41bd183380f051ce
jamieberrier <berrier.jamie@gmail.com>
2021-03-09 10:30:44 -0800

    Delete grants.js

--- grant-data/src/services/grants.js
+++
@@ -1,5 +0,0 @@
-// access the data and to convert the response into JSON 
-export function fetchGrants() {
-  return fetch('https://www.sbir.gov/api/solicitations.json?keyword=sbir')
-    .then(response => response.json())
-}
