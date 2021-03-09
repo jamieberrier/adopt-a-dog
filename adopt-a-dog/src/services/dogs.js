15c7ca137bc622beaeaf45cb838e43feda5cb315
jamieberrier <berrier.jamie@gmail.com>
2021-03-09 14:32:49 -0800

    Create dogs.js

---
+++ adopt-a-dog/src/services/dogs.js
@@ -0,0 +1,39 @@
+// access the data and to convert the response into JSON 
+export function fetchDogs() {
+  let bodyData = {
+    'apikey': process.env.REACT_APP_RESCUE_GROUPS_API_KEY,
+    'objectType': 'animals',
+    'objectAction': 'publicSearch',
+    'search': {
+      'resultStart': '0',
+      'resultLimit': '100',
+      'resultSort': 'animalID',
+      'resultOrder': 'asc',
+      'filters':
+      [
+        {
+          'fieldName': 'animalSpecies',
+          'operation': 'equals',
+          'criteria': 'Dog'
+        },
+        {
+          'fieldName': 'animalStatus',
+          'operation': 'equals',
+          'criteria': 'Available'
+        }
+      ],
+      'fields':
+      [
+        'animalID','animalName', 'animalBreed', 'animalDescription', 'animalSex', 'animalThumbnailUrl', 'animalLocation', 'animalGeneralAge'
+      ]
+    }
+  }
+
+  let configObject = {
+    method: 'POST',
+    body: JSON.stringify(bodyData)
+  }
+
+  return fetch('https://api.rescuegroups.org/http/v2.json', configObject)
+    .then(response => response.json())
+}
